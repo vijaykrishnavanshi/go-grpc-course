ab12df2d39b7193b5e25e7a00d79afb26c220eb0
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2020-02-08 04:50:54 +0530

    Implement Server Streaming for gRPC

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/vijaykrishnavanshi/go-grpc-course/greet/greetpb"
@@ -17,7 +18,8 @@ func main() {
 	}
 	defer cc.Close()
 	c := greetpb.NewGreetServiceClient(cc)
-	doUnary(c)
+	// doUnary(c)
+	doServerStreaming(c)
 }
 
 func doUnary(c greetpb.GreetServiceClient) {
@@ -33,4 +35,28 @@ func doUnary(c greetpb.GreetServiceClient) {
 		log.Fatalf("Error: %v", err)
 	}
 	log.Printf("Response from greet: %v", res.Result)
+}
+
+func doServerStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do Server Streaming RPC")
+	req := &greetpb.GreetManyTimesRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Vijay",
+			LastName:  "Krishnavanshi",
+		},
+	}
+	resStream, err := c.GreetManyTimes(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while requesting server streaming: %v", err)
+	}
+	for {
+		msg, err := resStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error while recieving message: %v", err)
+		}
+		log.Printf("Response from GreetManyTimes: %v", msg.Result)
+	}
 }
